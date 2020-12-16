e388b9d8c08ea41d635c6bde6d382d2e9f422a5a
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-12-17 00:18:01 +0530

    Update Dec 16

--- AppContainer.js
+++ AppContainer.js
@@ -3,14 +3,14 @@ import NavBar from "./Components/NavBar.js";
 import SearchBar from "./Components/SearchBar.js";
 import TopMovies from './Components/TopMovies.js';
 import Jumbo from './Components/Jumbo.js';
-import TweetCard from './Components/TweetCard.js';
+import SideBar from './Components/SideBar.js';
 import LatestMovies from './Components/LatestMovies.js';
 
 export default function AppContainer() {
 	return (
 		<div className="container">
 			<div className="navBar"><NavBar /></div>
-			<div className="sideBar"><TweetCard /></div>
+			<div className="sideBar"><SideBar /></div>
 			<div className="latestBar"><LatestMovies /></div>
 			<div className="searchBar"><SearchBar /></div>
 			<div className="jumboBar"><Jumbo /></div>
