235ba3a06b55c6b1661662b7f1d9621d925b03b6
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-12-18 16:17:48 +0530

    Update AppContainer - added Router

--- AppContainer.js
+++ AppContainer.js
@@ -5,16 +5,22 @@ import TopMovies from './Components/TopMovies.js';
 import Jumbo from './Components/Jumbo.js';
 import SideBar from './Components/SideBar.js';
 import LatestMovies from './Components/LatestMovies.js';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
 export default function AppContainer() {
 	return (
 		<div className="container">
-			<div className="navBar"><NavBar /></div>
-			<div className="sideBar"><SideBar /></div>
-			<div className="latestBar"><LatestMovies /></div>
-			<div className="searchBar"><SearchBar /></div>
-			<div className="jumboBar"><Jumbo /></div>
-			<div className="topmoviesBar"><TopMovies /></div>
+			<Router>
+				<div className="navBar"><NavBar /></div>
+				<div className="sideBar"><SideBar /></div>
+				<div className="latestBar"><LatestMovies /></div>
+				<div className="searchBar"><SearchBar /></div>
+				<div className="jumboBar"><Jumbo /></div>
+				<div className="topmoviesBar"><TopMovies /></div>
+        
+      </Router>
 		</div>
 		)
 }
