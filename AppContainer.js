b0a15d7907e86f6c7ccabd98e3e1baaa80e58211
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-12-23 13:51:10 +0530

    Update AppContainer.js

--- AppContainer.js
+++ AppContainer.js
@@ -1,26 +1,36 @@
 import React from "react";
 import NavBar from "./Components/NavBar.js";
 import SearchBar from "./Components/SearchBar.js";
-import TopMovies from './Components/TopMovies.js';
-import Jumbo from './Components/Jumbo.js';
-import SideBar from './Components/SideBar.js';
-import LatestMovies from './Components/LatestMovies.js';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import TopMovies from "./Components/TopMovies.js";
+import Jumbo from "./Components/Jumbo.js";
+import SideBar from "./Components/SideBar.js";
+import LatestMovies from "./Components/LatestMovies.js";
+import { Router } from "react-router-dom";
+import History from "./Components/History.js";
 
 export default function AppContainer() {
-	return (
-		<div className="container">
-			<Router>
-				<div className="navBar"><NavBar /></div>
-				<div className="sideBar"><SideBar /></div>
-				<div className="latestBar"><LatestMovies /></div>
-				<div className="searchBar"><SearchBar /></div>
-				<div className="jumboBar"><Jumbo /></div>
-				<div className="topmoviesBar"><TopMovies /></div>
-        
+  return (
+    <div className="container">
+      <Router history={History}>
+        <div className="navBar">
+          <NavBar />
+        </div>
+        <div className="sideBar">
+          <SideBar />
+        </div>
+        <div className="latestBar">
+          <LatestMovies />
+        </div>
+        <div className="searchBar">
+          <SearchBar />
+        </div>
+        <div className="jumboBar">
+          <Jumbo />
+        </div>
+        <div className="topmoviesBar">
+          <TopMovies />
+        </div>
       </Router>
-		</div>
-		)
+    </div>
+  );
 }
