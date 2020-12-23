089d55bbaaf88f7d50348dc8c816c6a71448b3ae
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-12-23 13:50:34 +0530

    Update SearchBar.js

--- Components/SearchBar.js
+++ Components/SearchBar.js
@@ -1,55 +1,68 @@
-import React, { useState } from "react";
-import { useHistory } from 'react-router-dom';
-import MovieResults from './MovieResults.js';
+import React, { useState, useEffect } from "react";
+import MovieResults from "./MovieResults.js";
+import History from "./History.js";
 
-export default function SearchBar() {
-	const [query, setQuery] = useState('');
+export default function SearchBar(location) {
+  const [qname, setQname] = useState("");
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const history = useHistory();
 
-  
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const q = params.get("q");
 
-	const searchMovies = async (e) => {
-        e.preventDefault();
-                
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=338f4376f5a74bedfa6b37ea5c054802&language=en-US&query=${query}&page=1&include_adult=false`;
-        
-        try {
-            const res = await fetch(url);
-            const data  = await res.json();
-            setMovies(data.results);
-        }catch(err){
-            console.error(err);
-        }
+    setQname(q ? q : "Harry");
+    //eslint-disable-next-line
+  }, []);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=338f4376f5a74bedfa6b37ea5c054802&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setQname(query);
+      setMovies(data.results);
+      History.push("search?q=" + query);
+    } catch (err) {
+      console.error(err);
     }
 
-	return (
-		<>
-			<div className="explore">
-					<h1>Explore</h1>
-				
-					<form className="form" onSubmit={searchMovies} {...history.push(`?=${query}`)}>
+    setQuery("");
+  };
 
-          
-            <input className="input" type="text" name="query"
-						placeholder="Find Movies, TV shows, Celebrities and more..."
-						value={query} onChange={(e) => setQuery(e.target.value)} />
+  return (
+    <>
+      <div className="explore">
+        <h1>Explore</h1>
 
-              
-              <button className="button" type="submit">Search</button>
-              
+        <form className="form" onSubmit={searchMovies}>
+          <input
+            className="input"
+            type="text"
+            name="query"
+            placeholder="Find Movies, TV shows, Celebrities and more..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
 
-              
-					</form>
-				
+          {/* <button className="button" type="submit">Search</button> */}
 
-				<div className="card-list">
-							{movies.filter(movie => movie.poster_path).map(movie => (
-	                   <MovieResults movie={movie} key={movie.id} />
-	                ))}
-	      </div>
-	      
-			</div>
-		</>
-		)
+          <button className="button" type="submit">
+            Search
+          </button>
+        </form>
+
+        <div className="card-list">
+          {movies
+            .filter((movie) => movie.poster_path)
+            .map((movie) => (
+              <MovieResults movie={movie} key={movie.id} />
+            ))}
+        </div>
+      </div>
+    </>
+  );
 }
